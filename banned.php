4104e9d5d46548a1eaead516b4c0d32afb2c20fe
iceconey14 <147879255+iceconey14@users.noreply.github.com>
2025-04-14 14:54:04 -0700

    more paprika
    maybe even some pepper

---
+++ banned.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: monospace;
+            text-align: center;
+            margin-top: 20vh;
+        }
+    </style>
+</head>
+<body>
+    <h1>you are banned</h1>
+    <p>never come back again, we hate you, go to hell.</p>
+</body>
+</html>

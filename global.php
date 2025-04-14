b30f647a568a2c8e4e312315689e8594e2b37379
iceconey14 <147879255+iceconey14@users.noreply.github.com>
2025-04-14 14:51:32 -0700

    a bit of paprika
    banned ip added

--- global.php
+++ global.php
@@ -1,29 +1,40 @@
 <?php
-    $mysqli = new mysqli("127.0.0.1", "root", "", "retrotube");
-    session_start();
-
-    function idFromUser($nameuser){
-    	global $mysqli;
-    	$uid = 0;
-    	$username = $mysqli->escape_string($nameuser);
-		$statement = $mysqli->prepare("SELECT `id` FROM `users` WHERE `username` = ?");
-		$statement->bind_param("s", $username);
-		$statement->execute();
-		$result = $statement->get_result();
-		while($row = $result->fetch_assoc()){
-			$uid = (int)$row["id"];
-		}
-		$statement->close();
-		return $uid;
+$mysqli = new mysqli("put mysql host here", "database name", "password", "user");
+session_start();
+// again, its for horrible people
+if (!empty($_SERVER['REMOTE_ADDR'])) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $stmt = $mysqli->prepare("SELECT id FROM banned_ips WHERE ip_address = ?");
+    $stmt->bind_param("s", $ip);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        header("Location: /banned.php");
+        exit();
     }
+    $stmt->close();
+}
 
-    function getUserPic($uid){
-    	$userpic = (string)$uid;
-		if(file_exists("./pfp/".$userpic) !== TRUE){
-			$userpic = "default";
-		}
-		return $userpic;
-    }
-    
-    $loggedIn = isset($_SESSION['profileuser3']);
+function idFromUser($nameuser){
+	global $mysqli;
+	$uid = 0;
+	$username = $mysqli->real_escape_string($nameuser);
+	$statement = $mysqli->prepare("SELECT id FROM users WHERE username = ?");
+	$statement->bind_param("s", $username);
+	$statement->execute();
+	$statement->bind_result($uid);
+	$statement->fetch();
+	$statement->close();
+	return (int)$uid;
+}
+
+function getUserPic($uid){
+	$userpic = (string)$uid;
+	if (!file_exists("./pfp/" . $userpic)) {
+		$userpic = "default";
+	}
+	return $userpic;
+}
+
+$loggedIn = isset($_SESSION['profileuser3']);
 ?>
